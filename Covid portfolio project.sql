437caf5f533a09933fc7ebb8b739beff406769c5
Daniyyel <funsoakinokun@gmail.com>
2021-11-12 07:29:33 +0400

    Capitalized Sql Keywords
    Capitalized Sql Keywords
    
    Capitalized SQL key words

--- Covid portfolio project.sql
+++ Covid portfolio project.sql
@@ -1,101 +1,100 @@
-SELECt LOCATION, date, total_cases, new_cases, total_deaths, population
-from `Covid deaths`
-order by 1,2 ;
+SELECT location, date, total_cases, new_cases, total_deaths, population
+FROM `Covid deaths`
+ORDER BY 1,2 ;
 
 -- Looking at total cases vs total deaths
 -- shows likelihood of dying if you contract covid in your country
-SELECt LOCATION, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
-from `Covid deaths`
-WHERE LOCATION like '%Nigeria%'
-order by 1,2 ;
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as Death_Percentage
+FROM `Covid deaths`
+WHERE Location LIKE '%Nigeria%'
+ORDER BY 1,2 ;
 
 -- Looking at the total cases vs the population
 -- shows what percentage of population got covid
-SELECt LOCATION, date, population, total_cases, (total_cases/population)*100 as infection_Percentage
-from `Covid deaths`
-WHERE LOCATION like '%Nigeria%'
-order by 1,2 ;
+SELECT Location, date, population, total_cases, (total_cases/population)*100 as infection_Percentage
+FROM `Covid deaths`
+WHERE Location LIKE '%Nigeria%'
+ORDER BY 1,2 ;
 
 
 -- Looking at countries with highest infection rates compared to population
-SELECt LOCATION, population, max(total_cases) as Highest_Infection_Count, max(total_cases/population)*100 as infection_Percentage
-from `Covid deaths`
-GROUP by LOCATION, population
-order by infection_Percentage desc ;
+SELECT Location, population, MAX(total_cases) as Highest_Infection_Count, MAX(total_cases/population)*100 as infection_Percentage
+FROM `Covid deaths`
+GROUP BY location, population
+ORDER BY infection_Percentage DESC ;
 
 -- Looking at countries with highest death count compared to population
-SELECt LOCATION, max(total_deaths) as Total_death_count
--- , max(total_deaths/population)*100 as death_Percentage
-from `Covid deaths`
+SELECT location, MAX(total_deaths) as Total_death_count
+-- , MAX(total_deaths/population) * 100 as death_Percentage
+FROM `Covid deaths`
 WHERE continent != ''
-GROUP by LOCATION
-order by Total_death_count desc ;
+GROUP BY LOCATION
+ORDER BY Total_death_count DESC ;
 
 -- let's break things down by continent
 -- Showing continent with the highest death count
-SELECt continent, max(total_deaths) as Total_death_count, max(total_deaths/population)*100 as death_Percentage
-from `Covid deaths`
+SELECT continent, MAX(total_deaths) as Total_death_count, MAX(total_deaths / population) * 100 AS death_Percentage
+FROM `Covid deaths`
 WHERE continent != ''
-GROUP by continent
-order by Total_death_count desc ;
+GROUP BY continent
+ORDER BY Total_death_count DESC ;
 
 
 -- Global numbers
 -- Death percentage by date
-SELECT date, sum(new_cases) as total_cases, sum(new_deaths) as total_deaths, (sum(new_deaths)/sum(new_cases)*100) as GlobalDeathPercentage
-from `Covid deaths`
+SELECT date, SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, (SUM(new_deaths)/SUM(new_cases)*100) as GlobalDeathPercentage
+FROM `Covid deaths`
 WHERE continent != ''
-GROUP by date 
-order by 1,2 ;
+GROUP BY date 
+ORDER BY 1,2 ;
 
 
 -- Global death percentage
-SELECT sum(new_cases) as total_cases, sum(new_deaths) as total_deaths, (sum(new_deaths)/sum(new_cases)*100) as GlobalDeathPercentage
-from `Covid deaths`
+SELECT SUM(new_cases) as total_cases, SUM(new_deaths) AS total_deaths, (SUM(new_deaths)/SUM(new_cases)*100) as GlobalDeathPercentage
+FROM `Covid deaths`
 WHERE continent != ''
-order by 1,2 ;
+ORDER BY 1,2 ;
 
 
 
 -- Working with covd vaccination
 SELECT * 
-from `Covid vaccinations` cv
-join `Covid deaths` cd 
-on cv.`location` = cd.`location`
-and cv.`date` = cd.`date`;
+FROM `Covid vaccinations` cv
+JOIN `Covid deaths` cd 
+ON cv.`location` = cd.`location`
+AND cv.`date` = cd.`date`;
 
 -- Looking at total population vs vaccination
-select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
-sum(cv.new_vaccinations) over (partition by cd.`location` order by cd.`location`, cd.`date`) as RollingPeopleVaccinated,
-from `Covid deaths` cd
-join `Covid vaccinations` cv 
-on cv.`location` = cd.`location`
-and cv.`date` = cd.`date`
+SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
+SUM(cv.new_vaccinations) OVER (PARTITION BY cd.`location` ORDER BY cd.`location`, cd.`date`) AS RollingPeopleVaccinated,
+FROM `Covid deaths` cd
+JOIN `Covid vaccinations` cv 
+ON cv.`location` = cd.`location`
+AND cv.`date` = cd.`date`
 WHERE cd.continent != ''
-and cd.`date` >= '2021-09-01';
+AND cd.`date` >= '2021-09-01';
 
 
 -- Using a CTE
-With PopulationVsVaccination (Continent, Location, Date, Population, new_vaccinations, RollingPeopleVaccinated)
-as 
+WITH PopulationVsVaccination (Continent, Location, Date, Population, new_vaccinations, RollingPeopleVaccinated)
+AS 
 (
-select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
-sum(cv.new_vaccinations) over (partition by cd.`location` order by cd.`location`, cd.`date`) as RollingPeopleVaccinated
-from `Covid deaths` cd
-join `Covid vaccinations` cv 
-on cv.`location` = cd.`location`
-and cv.`date` = cd.`date`
+SELECT cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations, 
+SUM(cv.new_vaccinations) OVER (PARTITION BY cd.`location` ORDER BY cd.`location`, cd.`date`) AS RollingPeopleVaccinated
+FROM `Covid deaths` cd
+JOIN `Covid vaccinations` cv 
+ON cv.`location` = cd.`location`
+AND cv.`date` = cd.`date`
 WHERE cd.continent != ''
-and cd.`date` >= '2021-09-01'
+AND cd.`date` >= '2021-09-01'
 )
-SELECT *, (RollingPeopleVaccinated/population)*100 
-From PopulationVsVaccination
-;
+SELECT *, (RollingPeopleVaccinated / population) * 100 
+FROM PopulationVsVaccination;
 
 
 -- temp table
-Drop Table if exists PercentPopulationVaccinated 
-Create TABLE PercentPopulationVaccinated
+DROP TABLE IF EXISTS PercentPopulationVaccinated 
+CREATE TABLE PercentPopulationVaccinated
 (
 Continent VARCHAR(255),
 Location VARCHAR(255),
@@ -106,29 +105,26 @@ RollingPeopleVaccinated VARCHAR(255)
 );
 
 insert into PercentPopulationVaccinated
-select cd.continent, cd.location, cd.date, cd.population, (cv.new_vaccinations), 
-sum((cv.new_vaccinations)) over (partition by cd.`location` order by cd.`location`, cd.`date`) as RollingPeopleVaccinated
-from `Covid deaths` cd
-join `Covid vaccinations` cv 
-on cv.`location` = cd.`location`
-and cv.`date` = cd.`date`
+SELECT cd.continent, cd.location, cd.date, cd.population, (cv.new_vaccinations), 
+SUM((cv.new_vaccinations)) OVER (PARTITION BY cd.`location` ORDER BY cd.`location`, cd.`date`) AS RollingPeopleVaccinated
+FROM `Covid deaths` cd
+JOIN `Covid vaccinations` cv 
+ON cv.`location` = cd.`location`
+AND cv.`date` = cd.`date`
 WHERE cd.continent != '';
 
 
-SELECT *, (RollingPeopleVaccinated/population)*100 
-From PercentPopulationVaccinated;
+SELECT *, (RollingPeopleVaccinated/population) * 100 
+FROM PercentPopulationVaccinated;
 
 -- Create view
-Create view Percent_PeopleVaccinated as 
-select cd.continent, cd.location, cd.date, cd.population, (cv.new_vaccinations), 
-sum((cv.new_vaccinations)) over (partition by cd.`location` order by cd.`location`, cd.`date`) as RollingPeopleVaccinated
-from `Covid deaths` cd
-join `Covid vaccinations` cv 
-on cv.`location` = cd.`location`
-and cv.`date` = cd.`date`
+CREATE VIEW Percent_PeopleVaccinated AS 
+SELECT cd.continent, cd.location, cd.date, cd.population, (cv.new_vaccinations), 
+SUM((cv.new_vaccinations)) over (PARTITION BY cd.`location` ORDER BY cd.`location`, cd.`date`) AS RollingPeopleVaccinated
+FROM `Covid deaths` cd
+JOIN `Covid vaccinations` cv 
+ON cv.`location` = cd.`location`
+AND cv.`date` = cd.`date`
 WHERE cd.continent != '';
 
 -----------------------------------------------
-
-
-
